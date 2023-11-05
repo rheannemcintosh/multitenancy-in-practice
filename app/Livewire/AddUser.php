375844b0d6afa301a726cbe207385c02e02f42c9
rheannemcintosh <rheannemcintosh@gmail.com>
2023-11-05 17:24:43 +0000

    Create a new user, and save the image to S3

--- app/Livewire/AddUser.php
+++ app/Livewire/AddUser.php
@@ -2,10 +2,15 @@
 
 namespace App\Livewire;
 
+use App\Models\User;
+use Illuminate\Support\Str;
 use Livewire\Component;
+use Livewire\WithFileUploads;
 
 class AddUser extends Component
 {
+    use WithFileUploads;
+
     public $name = "Kevin McKee";
     public $email = "kevin@lc.com";
     public $department = 'information_technology';
@@ -25,6 +30,21 @@ class AddUser extends Component
             'role' => 'required|string',
             'photo' => 'image|max:1024', // 1MB Max
         ]);
+
+        $filename = $this->photo->store('photos', 's3-public');
+
+        User::create([
+            'name' => $this->name,
+            'email' => $this->email,
+            'department' => $this->department,
+            'title' => $this->title,
+            'status' => $this->status,
+            'role' => $this->role,
+            'photo' => $filename,
+            'password' => bcrypt(Str::random(16)),
+        ]);
+
+        session()->flash('success', 'We Did It');
     }
 
     public function render()
